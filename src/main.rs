1e6637e87a639a6cd0445b7badfdd9ac71f361c2
al <alfredo.yap@stu.bmcc.cuny.edu>
2023-02-26 13:03:50 -0500

    conditions and control flow

--- src/main.rs
+++ src/main.rs
@@ -1,49 +1,23 @@
-use std::io;
-
 fn main() {
-    println!("Hello World!");
+    let cond = 2.1 <= 2.005;
 
-    add_to_number(5.0, 8.8);
-    minus_to_number(9.9, 6.834);
-    product_of_numbers(5, 5);
-    quotient_of_numbers(44, 22);
+    print!("{}", cond);
 
-    let x = 254_i16;
-    let y = 10_i32;
+    let cond2 = true && cond;
 
-    let z = x % y as i16;
-    println!("{}", z);
+    println!("{}", cond2);
 
-    let xx = (i32::MAX as f32) + 1.0;
-    println!("{}", xx);
+    let cond3 = false || cond2;
 
-    let mut input2 = String::new();
-    io::stdin()
-        .read_line(&mut input2)
-        .expect("expected to read line");
+    println!("{}", cond3);
 
-    let parse_input2_to_i64: i64 = input2.trim().parse().unwrap();
+    let cond4 = "bread";
 
-    println!("Enter an input");
-    println!("Your input ist: {}", parse_input2_to_i64);
-}
-
-fn add_to_number(x: f32, y: f32) {
-    let sum_of_two_nums = x + y;
-    println!("The sum of {} and {} is: {}", x, y, sum_of_two_nums);
-}
-
-fn minus_to_number(x: f32, y: f32) {
-    let difference_value: f32 = x - y;
-    println!("The difference of {} and {} is: {}", x, y, difference_value);
-}
-
-fn product_of_numbers(x: i32, y: i32) {
-    let product_value: i32 = x * y;
-    println!("The product of {} and {} is: {}", x, y, product_value);
-}
-
-fn quotient_of_numbers(x: i64, y: i64) {
-    let quotient_value: i64 = x / y;
-    println!("The quotient of {} and {} is: {}", x, y, quotient_value);
+    if cond4 == "food" {
+        print!("Your condition is met!");
+    } else if cond4 == "cake" {
+        print!("That was a good coake");
+    } else {
+        print!("Chocolate Crepe");
+    }
 }
